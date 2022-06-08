50a4a7c42c7b4639ec4eddd59b532c87131f9806
TahaKhan <Muhammad.Taha@hysabkytab.com>
2022-06-08 16:58:19 +0500

    description off

--- Producer/src/Account/Account.controller.ts
+++ Producer/src/Account/Account.controller.ts
@@ -3,24 +3,23 @@ import { AccountDto } from './dto/Account.dto';
 import { AccountService } from './Account.service';
 import { ResponseWrapper } from 'src/common/enums/response-wrapper';
 import { StatusCodes } from '../common/enums/status-codes';
-import { AccountRepository } from './Account.repository';
 
 @Controller('Account')
 export class AccountController {
     constructor(
         private readonly service: AccountService,
-        private readonly repo: AccountRepository
     ) { }
 
     @Post('/create')
-    async createAccount(@Body() AccountDto): Promise<any> {
+    async createAccount(@Body() AccountDto: AccountDto[]): Promise<any> {
         var response = new ResponseWrapper<any>();
         try {
-            let decryptDto = await this.repo.decryptText(AccountDto.u, "34BC51A6046A624881701EFD17115CBA")
-            let data = await this.service.CreateAccountServiceHandler(JSON.parse(decryptDto));
+            // let decryptDto = await this.repo.decryptText(AccountDto.u, "34BC51A6046A624881701EFD17115CBA")
+            // let data = await this.service.CreateAccountServiceHandler(JSON.parse(decryptDto));
+            let data = await this.service.CreateAccountServiceHandler(AccountDto);
             response.StatusCode = StatusCodes.Success;
             response.Result = data;
-            response.Message = 'Account Created'
+            response.Message = 'Account Produce In kafka'
         } catch (error) {
             response.Result = null;
             response.StatusCode = StatusCodes.Exception;
